63e3d5287a24c12e419d29eb10a4e85feae63b49
Ali Kamali Sarvestani <a.kamali.srv@gmail.com>
2026-05-28 23:09:35 +0330

    chore(app): add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+	
+  publicDir: 'public',
+  
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
+  
+  server: {
+	open: true,
+  },
+
+   build: {
+    outDir: 'build',
+    emptyOutDir: true,
+  },
+
+});
