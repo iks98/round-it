0725412490a48c77fe7a171d02c74fa89f81c7c9
Ali Kamali Sarvestani <a.kamali.srv@gmail.com>
2026-07-18 23:19:12 +0330

    chore(app): add tailwind css

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'wxt';
+import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
 });
