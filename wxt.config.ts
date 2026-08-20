b9a5eb6b01fa6fffb0933f644da61c46455b1b97
Ali Kamali Sarvestani <a.kamali.srv@gmail.com>
2026-08-20 05:16:10 +0330

    chore(app): add vueJs

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'wxt';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
   vite: () => ({
-    plugins: [tailwindcss()],
+    plugins: [vue(), tailwindcss()],
   }),
 });
